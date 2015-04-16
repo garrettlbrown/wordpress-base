7274b2a6a07d4d7062c5482ba4324a40ead1c569
Josh Lee <josh@daftlabs.com>
2015-04-16 16:49:40 -0400

    makes wordpress cookbook support multisite

--- cookbooks/wordpress/attributes/default.rb
+++ cookbooks/wordpress/attributes/default.rb
@@ -9,4 +9,7 @@ default['mysql']['bind_address']                  = "localhost"
 default['wordpress']['themes_directory']          = "/var/www/wordpress-base/themes" 
 default['wordpress']['content_directory']         = "/var/www/wordpress-base/wp-content" 
 default['wordpress']['name']                      = "wordpress-base"
-default['database']['prefix'] = 'wp_'
+default['database']['prefix']                     = 'wp_'
+
+default['multisite']                              = false
+default['multisite_domain']                       = 'mysite.com'
